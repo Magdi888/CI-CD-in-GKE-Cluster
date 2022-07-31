39fe223b02d953c55635c17ad08c506e2964889c
Ahmed Magdi <a.magdi888@gmail.com>
2022-07-31 16:53:11 +0200

    edit path of docker file

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@
 
 def BuildImage() {
     withCredentials([usernamePassword(credentialsId :'DockerHub',usernameVariable :'USER',passwordVariable :'PASSWORD')]){
-       sh 'docker build -t amagdi888/my-repo:Python-App -f ./App/Dockerfile .'
+       sh 'docker build -t amagdi888/my-repo:Python-App -f ./App/Dockerfile ./App'
        sh 'echo $PASSWORD | docker login -u $USER --password-stdin'
        sh 'docker push amagdi888/my-repo:Python-App'
     }
